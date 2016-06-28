350d23bcce4af5b377d96e0c31f1022d8595ab4a
luxiaoyuan <luxy@xiaomodo.com>
2016-06-28 16:45:53 +0800

    reward view fix

--- app/src/main/java/com/xmd/technician/chat/chatview/ChatViewReward.java
+++ app/src/main/java/com/xmd/technician/chat/chatview/ChatViewReward.java
@@ -1,6 +1,7 @@
 package com.xmd.technician.chat.chatview;
 
 import android.content.Context;
+import android.text.Spannable;
 import android.view.LayoutInflater;
 import android.widget.TextView;
 
@@ -8,6 +9,8 @@ import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMTextMessageBody;
 import com.xmd.technician.R;
 import com.xmd.technician.chat.ChatConstant;
+import com.xmd.technician.chat.CommonUtils;
+import com.xmd.technician.chat.SmileUtils;
 
 /**
  * Created by sdcm on 16-4-12.
@@ -15,6 +18,7 @@ import com.xmd.technician.chat.ChatConstant;
 public class ChatViewReward extends BaseChatView{
 
     private TextView mRewardTip;
+    private TextView mRewardValue;
     public ChatViewReward(Context context, EMMessage.Direct direct) {
         super(context, direct);
     }
@@ -28,12 +32,16 @@ public class ChatViewReward extends BaseChatView{
     @Override
     protected void onFindViewById() {
         mRewardTip = (TextView) findViewById(R.id.reward_tip);
+        mRewardValue = (TextView) findViewById(R.id.user_reward);
     }
 
     @Override
     protected void onSetUpView() {
-        userAvatarView.setVisibility(GONE);
-        timeStampView.setVisibility(GONE);
+        /*userAvatarView.setVisibility(GONE);
+        timeStampView.setVisibility(GONE);*/
+        Spannable span = SmileUtils.getSmiledText(context, CommonUtils.getMessageDigest(message, context));
+        // 设置内容
+        mRewardValue.setText(span, TextView.BufferType.SPANNABLE);
         mRewardTip.setText(String.format("%s的打赏已存入您的账户", message.getStringAttribute(ChatConstant.KEY_NAME,"")));
     }
 }
