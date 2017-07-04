843a7e13b2fd338eda59ee049fa51d6ba501d410
lianghongjing <lianghj@xiaomodo.com>
2017-07-04 17:40:09 +0800

    修改技师海报1.7.8 对外发布版本 更换图片

--- xmd-tech/src/main/java/com/xmd/technician/SharedPreferenceHelper.java
+++ xmd-tech/src/main/java/com/xmd/technician/SharedPreferenceHelper.java
@@ -187,8 +187,8 @@ public class SharedPreferenceHelper {
      */
     public static String getServerHost() {
      //   return "http://192.168.2.74:8080";
-      return "http://192.168.1.100:9880";
-    //   return mSettingPreference.getString(KEY_SERVER_HOST, RequestConstant.SERVER_HOST);
+     // return "http://192.168.1.100:9880";
+       return mSettingPreference.getString(KEY_SERVER_HOST, RequestConstant.SERVER_HOST);
     }
 
     public static void setLastAutoCheckUpgrade(long mill) {
