3ccd9d7fec461b79736ae110d5fffdea03d2afbd
zhangrong <zhangrong@xiaomodo.com>
2017-10-12 19:55:20 +0800

    [xmd-cashier]更新版本号; 统计数据支持long

--- xmd-cashier/src/main/java/com/xmd/cashier/dal/bean/OfflineStatisticInfo.java
+++ xmd-cashier/src/main/java/com/xmd/cashier/dal/bean/OfflineStatisticInfo.java
@@ -5,19 +5,19 @@ package com.xmd.cashier.dal.bean;
  */
 
 public class OfflineStatisticInfo {
-    public int aliMember;    //【子】会员充值-支付宝	number
-    public int cashMember;    //【子】会员充值-现金	number
-    public int cashPos;    //pos现金	number
-    public int otherMember;    //【子】会员充值-其他	number
-    public int recordCount; //记录数;	number
-    public int totalAmount;    //详细列表的总值	number
-    public int totalDiscount;//	优惠减免	number
-    public int totalMember;    //会员充值订单金额	number
-    public int totalPos;    //pos现金支付订单金额	number
-    public int totalRecharge;//	充值收入	number
-    public int totalRefund;    //退款金额	number
-    public int totalRefundOther;//	【子】退款其他	number
-    public int totalSubtract;    //【子】错充扣除	number
-    public int unionMember;    //【子】会员充值-银联	number
-    public int wxMember;    //【子】会员充值-微信	number
+    public long aliMember;    //【子】会员充值-支付宝	number
+    public long cashMember;    //【子】会员充值-现金	number
+    public long cashPos;    //pos现金	number
+    public long otherMember;    //【子】会员充值-其他	number
+    public long recordCount; //记录数;	number
+    public long totalAmount;    //详细列表的总值	number
+    public long totalDiscount;//	优惠减免	number
+    public long totalMember;    //会员充值订单金额	number
+    public long totalPos;    //pos现金支付订单金额	number
+    public long totalRecharge;//	充值收入	number
+    public long totalRefund;    //退款金额	number
+    public long totalRefundOther;//	【子】退款其他	number
+    public long totalSubtract;    //【子】错充扣除	number
+    public long unionMember;    //【子】会员充值-银联	number
+    public long wxMember;    //【子】会员充值-微信	number
 }
--- xmd-cashier/src/main/java/com/xmd/cashier/dal/bean/OnlineStatisticInfo.java
+++ xmd-cashier/src/main/java/com/xmd/cashier/dal/bean/OnlineStatisticInfo.java
@@ -5,27 +5,27 @@ package com.xmd.cashier.dal.bean;
  */
 
 public class OnlineStatisticInfo {
-    public int fastPay; // 在线买单收入 number
-    public int internalCommission;  // 优惠减免 number
-    public int itemCard;    //项目次卡 number
-    public int itemPackage; // 混合套餐 number
-    public int mall;    // 商城收入 number
-    public int marketing;   // 营销活动 number
-    public int paidCoupon;  // 点钟券 number
-    public int paidOrder;   // 付费预约 number
-    public int paidServiceItem; // 限时抢 number
-    public int qrClub;  //水牌买单 number
-    public int qrPos;   // POS买单 number
-    public int qrTech;  // 扫工牌买单 number
-    public int recharge;    // 会员充值 number
-    public int totalAli;    // 支付宝 number
-    public int totalAmount; // 交易总金额 number
-    public int totalDiscount;   // 用券抵扣 number
-    public int totalManagerAmount;  // 会所部分 number
-    public int totalManagerGetAmount;   // 实际结算金额 number
-    public int totalRefund; // 退款 number
-    public int totalSettleAmount;   // 应结算金额 number 光大或代付！
-    public int totalSettleFee;  // 手续费 number
-    public int totalUnion;  // 银联 number
-    public int totalWx; // 微信 number
+    public long fastPay; // 在线买单收入 number
+    public long internalCommission;  // 优惠减免 number
+    public long itemCard;    //项目次卡 number
+    public long itemPackage; // 混合套餐 number
+    public long mall;    // 商城收入 number
+    public long marketing;   // 营销活动 number
+    public long paidCoupon;  // 点钟券 number
+    public long paidOrder;   // 付费预约 number
+    public long paidServiceItem; // 限时抢 number
+    public long qrClub;  //水牌买单 number
+    public long qrPos;   // POS买单 number
+    public long qrTech;  // 扫工牌买单 number
+    public long recharge;    // 会员充值 number
+    public long totalAli;    // 支付宝 number
+    public long totalAmount; // 交易总金额 number
+    public long totalDiscount;   // 用券抵扣 number
+    public long totalManagerAmount;  // 会所部分 number
+    public long totalManagerGetAmount;   // 实际结算金额 number
+    public long totalRefund; // 退款 number
+    public long totalSettleAmount;   // 应结算金额 number 光大或代付！
+    public long totalSettleFee;  // 手续费 number
+    public long totalUnion;  // 银联 number
+    public long totalWx; // 微信 number
 }
