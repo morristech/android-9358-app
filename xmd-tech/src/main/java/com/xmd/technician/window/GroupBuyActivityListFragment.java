3f01503074cf7d1ed4e1cc20fc5dd353d19fb790
lianghongjing <lianghj@xiaomodo.com>
2018-01-19 19:21:20 +0800

    修改分享文案

--- xmd-tech/src/main/java/com/xmd/technician/window/GroupBuyActivityListFragment.java
+++ xmd-tech/src/main/java/com/xmd/technician/window/GroupBuyActivityListFragment.java
@@ -127,10 +127,10 @@ public class GroupBuyActivityListFragment extends BaseListFragment<GroupBuyBean>
     }
 
     private String getDescribeMessage(GroupBuyBean bean){
-//        String itemName = bean.itemName;
-//        String itemPrice = String.format("原价%1.2f元",bean.itemPrice / 100f );
-//        String price = String.format("拼团价%1.2f元",bean.price / 100f);
-//        return String.format("%s,%s,%s,点击参与优惠",itemName,itemPrice,price);
-        return ResourceUtils.getString(R.string.group_buy_share_des);
+        String itemName = bean.itemName;
+        String itemPrice = String.format("原价%1.2f元",bean.itemPrice / 100f );
+        String price = String.format("拼团价%1.2f元",bean.price / 100f);
+        return String.format(ResourceUtils.getString(R.string.group_buy_share_des)+"%s,%s,%s,点击参与优惠",itemName,itemPrice,price);
+
     }
 }
